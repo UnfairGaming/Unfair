05257ab92203c096615bee549dabdd1fd8e066ff
UnfairGaming <3675967734@qq.com>
2026-08-03 19:07:54 +0800

    config bypass

--- src/main/java/cn/unfair/config/Config.java
+++ src/main/java/cn/unfair/config/Config.java
@@ -3,40 +3,28 @@ package cn.unfair.config;
 import com.google.gson.*;
 import net.minecraft.client.Minecraft;
 import cn.unfair.Unfair;
-import cn.unfair.event.EventTarget;
-import cn.unfair.event.types.EventType;
-import cn.unfair.events.TickEvent;
 import cn.unfair.mixin.IAccessorMinecraft;
 import cn.unfair.module.Module;
 import cn.unfair.property.Property;
 import cn.unfair.util.ChatUtil;
 
 import java.io.*;
-import java.nio.file.AtomicMoveNotSupportedException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 
 public class Config {
-    public static final Minecraft mc = Minecraft.getMinecraft();
-    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    public static Minecraft mc = Minecraft.getMinecraft();
+    public static Gson gson = new GsonBuilder().setPrettyPrinting().create();
     public static String lastConfig;
-    private static final long AUTO_SAVE_DELAY_MS = 1000L;
-    private static Config currentConfig;
-    private static boolean initialized = false;
-    private static boolean loading = false;
-    private static boolean dirty = false;
-    private static long lastDirtyTime = 0L;
     public String name;
     public File file;
 
     public Config(String name, boolean newConfig) {
         this.name = name;
+        lastConfig = name;
         if (name.equals("!") || name.equals("default")) {
             this.name = "default";
         }
-        lastConfig = this.name;
-        this.file = new File(new File(mc.mcDataDir, "config/Unfair"), String.format("%s.json", this.name));
+        this.file = new File("./config/Unfair/", String.format("%s.json", this.name));
         try {
             file.getParentFile().mkdirs();
             if (newConfig) {
@@ -49,11 +37,6 @@ public class Config {
 
     public void load() {
         try {
-            if (dirty && currentConfig != null && !currentConfig.file.equals(this.file)) {
-                saveCurrentSilent();
-            }
-            currentConfig = this;
-            lastConfig = this.name;
 
             if (!file.exists()) {
                 ChatUtil.sendFormatted(String.format("%sConfig file not found (&c&o%s&r). Creating default config...&r", Unfair.clientName, file.getName()));
@@ -61,61 +44,52 @@ public class Config {
                 return;
             }
 
-            JsonElement parsed;
-            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                parsed = new JsonParser().parse(reader);
-            }
+            JsonElement parsed = new JsonParser().parse(new BufferedReader(new FileReader(file)));
             if (parsed == null || !parsed.isJsonObject()) {
                 ChatUtil.sendFormatted(String.format("%sInvalid config format (&c&o%s&r)&r", Unfair.clientName, file.getName()));
                 return;
             }
 
             JsonObject jsonObject = parsed.getAsJsonObject();
-            loading = true;
-            try {
-                for (Module module : Unfair.moduleManager.modules.values()) {
-                    JsonElement moduleObj = jsonObject.get(module.getName());
-                    if (moduleObj != null && moduleObj.isJsonObject()) {
-                        JsonObject object = moduleObj.getAsJsonObject();
+            for (Module module : Unfair.moduleManager.modules.values()) {
+                JsonElement moduleObj = jsonObject.get(module.getName());
+                if (moduleObj != null && moduleObj.isJsonObject()) {
+                    JsonObject object = moduleObj.getAsJsonObject();
 
-                        ArrayList<Property<?>> list = Unfair.propertyManager.properties.get(module.getClass());
-                        if (list != null) {
-                            for (Property<?> property : list) {
-                                if (object.has(property.getName())) {
-                                    try {
-                                        property.read(object);
-                                    } catch (Exception e) {
-                                        ((IAccessorMinecraft) mc).getLogger().warn(String.format("Failed to load property %s for module %s", property.getName(), module.getName()));
-                                    }
+                    ArrayList<Property<?>> list = Unfair.propertyManager.properties.get(module.getClass());
+                    if (list != null) {
+                        for (Property<?> property : list) {
+                            if (object.has(property.getName())) {
+                                try {
+                                    property.read(object);
+                                } catch (Exception e) {
+                                    ((IAccessorMinecraft) mc).getLogger().warn(String.format("Failed to load property %s for module %s", property.getName(), module.getName()));
                                 }
                             }
                         }
+                    }
 
-                        if (object.has("toggled")) {
-                            JsonElement toggled = object.get("toggled");
-                            if (toggled != null && toggled.isJsonPrimitive()) {
-                                module.setEnabled(toggled.getAsBoolean());
-                            }
+                    if (object.has("toggled")) {
+                        JsonElement toggled = object.get("toggled");
+                        if (toggled != null && toggled.isJsonPrimitive()) {
+                            module.setEnabled(toggled.getAsBoolean());
                         }
+                    }
 
-                        if (object.has("key")) {
-                            JsonElement key = object.get("key");
-                            if (key != null && key.isJsonPrimitive()) {
-                                module.setKey(key.getAsInt());
-                            }
+                    if (object.has("key")) {
+                        JsonElement key = object.get("key");
+                        if (key != null && key.isJsonPrimitive()) {
+                            module.setKey(key.getAsInt());
                         }
+                    }
 
-                        if (object.has("hidden")) {
-                            JsonElement hidden = object.get("hidden");
-                            if (hidden != null && hidden.isJsonPrimitive()) {
-                                module.setHidden(hidden.getAsBoolean());
-                            }
+                    if (object.has("hidden")) {
+                        JsonElement hidden = object.get("hidden");
+                        if (hidden != null && hidden.isJsonPrimitive()) {
+                            module.setHidden(hidden.getAsBoolean());
                         }
                     }
                 }
-            } finally {
-                loading = false;
-                dirty = false;
             }
             ChatUtil.sendFormatted(String.format("%sConfig has been loaded (&a&o%s&r)&r", Unfair.clientName, file.getName()));
         } catch (FileNotFoundException e) {
@@ -130,13 +104,7 @@ public class Config {
     }
 
     public void save() {
-        save(false);
-    }
-
-    private void save(boolean silent) {
         try {
-            currentConfig = this;
-            lastConfig = this.name;
             if (!file.getParentFile().exists()) {
                 file.getParentFile().mkdirs();
             }
@@ -161,69 +129,13 @@ public class Config {
                 object.add(module.getName(), moduleObject);
             }
 
-            File tempFile = new File(file.getParentFile(), file.getName() + ".tmp");
-            try (PrintWriter printWriter = new PrintWriter(new FileWriter(tempFile))) {
-                printWriter.println(gson.toJson(object));
-            }
-            try {
-                Files.move(tempFile.toPath(), file.toPath(), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.ATOMIC_MOVE);
-            } catch (AtomicMoveNotSupportedException e) {
-                Files.move(tempFile.toPath(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
-            }
-            dirty = false;
-            if (!silent) {
-                ChatUtil.sendFormatted(String.format("%sConfig has been saved (&a&o%s&r)&r", Unfair.clientName, file.getName()));
-            }
+            PrintWriter printWriter = new PrintWriter(new FileWriter(file));
+            printWriter.println(gson.toJson(object));
+            printWriter.close();
+            ChatUtil.sendFormatted(String.format("%sConfig has been saved (&a&o%s&r)&r", Unfair.clientName, file.getName()));
         } catch (IOException e) {
             ((IAccessorMinecraft) mc).getLogger().error("Error saving config: " + e.getMessage());
-            if (!silent) {
-                ChatUtil.sendFormatted(String.format("%sConfig couldn't be saved (&c&o%s&r)&r", Unfair.clientName, file.getName()));
-            }
-        }
-    }
-
-    public static void initAutosave(Config config) {
-        currentConfig = config;
-        lastConfig = config.name;
-        initialized = true;
-    }
-
-    public static void markDirty() {
-        if (!initialized || loading) {
-            return;
-        }
-        dirty = true;
-        lastDirtyTime = System.currentTimeMillis();
-    }
-
-    public static void markDirtyAndSave() {
-        markDirty();
-        if (dirty) {
-            saveCurrentSilent();
-        }
-    }
-
-    public static void saveCurrent() {
-        getCurrentConfig().save();
-    }
-
-    public static void saveCurrentSilent() {
-        getCurrentConfig().save(true);
-    }
-
-    private static Config getCurrentConfig() {
-        if (currentConfig == null) {
-            currentConfig = new Config(lastConfig == null ? "default" : lastConfig, false);
-        }
-        return currentConfig;
-    }
-
-    @EventTarget
-    public void onTick(TickEvent event) {
-        if (event.getType() == EventType.PRE
-                && dirty
-                && System.currentTimeMillis() - lastDirtyTime >= AUTO_SAVE_DELAY_MS) {
-            saveCurrentSilent();
+            ChatUtil.sendFormatted(String.format("%sConfig couldn't be saved (&c&o%s&r)&r", Unfair.clientName, file.getName()));
         }
     }
 }
--- src/main/java/cn/unfair/module/modules/movement/Fly.java
+++ src/main/java/cn/unfair/module/modules/movement/Fly.java
@@ -1,28 +1,94 @@
 package cn.unfair.module.modules.movement;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityBoat;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.C02PacketUseEntity;
+import net.minecraft.network.play.client.C0BPacketEntityAction;
+import net.minecraft.network.play.server.S32PacketConfirmTransaction;
 import cn.unfair.event.EventTarget;
 import cn.unfair.event.types.EventType;
+import cn.unfair.events.PacketEvent;
 import cn.unfair.events.StrafeEvent;
 import cn.unfair.events.UpdateEvent;
 import cn.unfair.module.Module;
 import cn.unfair.property.properties.FloatProperty;
+import cn.unfair.property.properties.ModeProperty;
 import cn.unfair.util.KeyBindUtil;
 import cn.unfair.util.MoveUtil;
+import cn.unfair.util.PacketUtil;
+import cn.unfair.util.TeamUtil;
+
+import java.util.Deque;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class Fly extends Module {
     private static final Minecraft mc = Minecraft.getMinecraft();
+    public final ModeProperty mode = new ModeProperty("mode", 0, new String[]{"VANILLA", "POLAR"});
     public final FloatProperty hSpeed = new FloatProperty("horizontal-speed", 1.0F, 0.0F, 100.0F);
     public final FloatProperty vSpeed = new FloatProperty("vertical-speed", 1.0F, 0.0F, 100.0F);
+    private final Deque<Packet<?>> polarS32Packets = new ConcurrentLinkedDeque<>();
     private double verticalMotion = 0.0;
+    private Entity polarBoat = null;
+    private boolean polarAttackedBoat = false;
+    private boolean polarMovementCheckDisabled = false;
 
     public Fly() {
         super("Fly", false);
     }
 
+    private boolean shouldUseVanillaFly() {
+        return this.mode.getValue() == 0 || this.polarMovementCheckDisabled;
+    }
+
+    private void resetPolar() {
+        this.polarBoat = null;
+        this.polarAttackedBoat = false;
+        this.polarMovementCheckDisabled = false;
+        this.polarS32Packets.clear();
+    }
+
+    private void flushPolarPackets() {
+        Packet<?> packet;
+        while ((packet = this.polarS32Packets.poll()) != null) {
+            PacketUtil.receivePacketNoEvent(packet);
+        }
+    }
+
+    private boolean isPolarBoatGone() {
+        return this.polarBoat == null
+                || this.polarBoat.isDead
+                || mc.theWorld == null
+                || !TeamUtil.isEntityLoaded(this.polarBoat);
+    }
+
+    private boolean isPolarDismountPacket(Packet<?> packet) {
+        return this.mode.getValue() == 1
+                && packet instanceof C0BPacketEntityAction
+                && ((C0BPacketEntityAction) packet).getAction() == C0BPacketEntityAction.Action.START_SNEAKING
+                && (mc.thePlayer.ridingEntity instanceof EntityBoat || this.polarBoat != null);
+    }
+
+    private void updatePolarState() {
+        if (this.mode.getValue() != 1 || mc.thePlayer == null) {
+            return;
+        }
+
+        if (!this.polarAttackedBoat && mc.thePlayer.ridingEntity instanceof EntityBoat) {
+            this.polarBoat = mc.thePlayer.ridingEntity;
+            PacketUtil.sendPacket(new C02PacketUseEntity(this.polarBoat, C02PacketUseEntity.Action.ATTACK));
+            this.polarAttackedBoat = true;
+        }
+
+        if (this.polarAttackedBoat && !this.polarMovementCheckDisabled && this.isPolarBoatGone()) {
+            this.polarMovementCheckDisabled = true;
+        }
+    }
+
     @EventTarget
     public void onStrafe(StrafeEvent event) {
-        if (this.isEnabled()) {
+        if (this.isEnabled() && this.shouldUseVanillaFly()) {
             if (mc.thePlayer.posY % 1.0 != 0.0) {
                 mc.thePlayer.motionY = this.verticalMotion;
             }
@@ -34,6 +100,10 @@ public class Fly extends Module {
     @EventTarget
     public void onUpdate(UpdateEvent event) {
         if (this.isEnabled() && event.getType() == EventType.PRE) {
+            this.updatePolarState();
+            if (!this.shouldUseVanillaFly()) {
+                return;
+            }
             this.verticalMotion = 0.0;
             if (mc.currentScreen == null) {
                 if (KeyBindUtil.isKeyDown(mc.gameSettings.keyBindJump.getKeyCode())) {
@@ -47,10 +117,41 @@ public class Fly extends Module {
         }
     }
 
+    @EventTarget
+    public void onPacket(PacketEvent event) {
+        if (this.isEnabled() && event.getType() == EventType.SEND && this.isPolarDismountPacket(event.getPacket())) {
+            event.setCancelled(true);
+            return;
+        }
+
+        if (this.isEnabled()
+                && this.mode.getValue() == 1
+                && this.polarAttackedBoat
+                && event.getType() == EventType.RECEIVE
+                && event.getPacket() instanceof S32PacketConfirmTransaction) {
+            this.polarS32Packets.offer(event.getPacket());
+            event.setCancelled(true);
+        }
+    }
+
+    @Override
+    public void onEnabled() {
+        this.resetPolar();
+    }
+
     @Override
     public void onDisabled() {
-        mc.thePlayer.motionY = 0.0;
+        this.flushPolarPackets();
+        this.resetPolar();
+        if (mc.thePlayer != null) {
+            mc.thePlayer.motionY = 0.0;
+        }
         MoveUtil.setSpeed(0.0);
         KeyBindUtil.updateKeyState(mc.gameSettings.keyBindSneak.getKeyCode());
+    }
+
+    @Override
+    public String[] getSuffix() {
+        return new String[]{this.mode.getModeString()};
     }
 }
