4cf05d201081bb9b8900f2059c37740f1dc93669
hhhhh <jincheng20032022@163.com>
2026-08-11 12:25:53 +0800

    [+] GrimOfHand NoSlow

--- src/main/java/cn/unfair/module/modules/movement/NoSlow.java
+++ src/main/java/cn/unfair/module/modules/movement/NoSlow.java
@@ -3,8 +3,10 @@ package cn.unfair.module.modules.movement;
 import cn.unfair.Unfair;
 import cn.unfair.enums.FloatModules;
 import cn.unfair.event.EventTarget;
+import cn.unfair.event.types.EventType;
 import cn.unfair.event.types.Priority;
 import cn.unfair.events.LivingUpdateEvent;
+import cn.unfair.events.PacketEvent;
 import cn.unfair.events.PlayerUpdateEvent;
 import cn.unfair.events.RightClickMouseEvent;
 import cn.unfair.module.Module;
@@ -13,6 +15,8 @@ import cn.unfair.property.properties.ModeProperty;
 import cn.unfair.property.properties.PercentProperty;
 import cn.unfair.util.BlockUtil;
 import cn.unfair.util.ItemUtil;
+import cn.unfair.util.KeyBindUtil;
+import cn.unfair.util.PacketUtil;
 import cn.unfair.util.PlayerUtil;
 import cn.unfair.util.TeamUtil;
 import com.google.common.base.CaseFormat;
@@ -20,14 +24,23 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.C07PacketPlayerDigging;
+import net.minecraft.network.play.client.C0DPacketCloseWindow;
+import net.minecraft.network.play.client.C0FPacketConfirmTransaction;
+import net.minecraft.network.play.client.CPacketSwapItemWithOffHand;
+import net.minecraft.network.play.server.S08PacketPlayerPosLook;
+import net.minecraft.network.play.server.S2FPacketSetSlot;
 import net.minecraft.util.BlockPos;
+
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class NoSlow extends Module {
     private static final Minecraft mc = Minecraft.getMinecraft();
     public final ModeProperty swordMode = new ModeProperty("sword-mode", 1, new String[]{"NONE", "VANILLA"});
     public final PercentProperty swordMotion = new PercentProperty("sword-motion", 100, () -> this.swordMode.getValue() != 0);
     public final BooleanProperty swordSprint = new BooleanProperty("sword-sprint", true, () -> this.swordMode.getValue() != 0);
-    public final ModeProperty foodMode = new ModeProperty("food-mode", 0, new String[]{"NONE", "VANILLA", "FLOAT"});
+    public final ModeProperty foodMode = new ModeProperty("food-mode", 0, new String[]{"NONE", "VANILLA", "FLOAT", "C0F"});
     public final PercentProperty foodMotion = new PercentProperty("food-motion", 100, () -> this.foodMode.getValue() != 0);
     public final BooleanProperty foodSprint = new BooleanProperty("food-sprint", true, () -> this.foodMode.getValue() != 0);
     public final ModeProperty bowMode = new ModeProperty("bow-mode", 0, new String[]{"NONE", "VANILLA", "FLOAT"});
@@ -35,8 +48,29 @@ public class NoSlow extends Module {
     public final BooleanProperty bowSprint = new BooleanProperty("bow-sprint", true, () -> this.bowMode.getValue() != 0);
     private int lastSlot = -1;
 
+    private enum C0FStep {NONE, CANCEL_C0F, SWAP_HANDS, EATING}
+
+    private C0FStep c0fStep = C0FStep.NONE;
+    private int c0fNoUsingItemTicks = 0;
+    private final LinkedBlockingQueue<Packet<?>> c0fPackets = new LinkedBlockingQueue<>();
+
+    public static boolean fakeEating = false;
+
     public NoSlow() {
         super("NoSlow", false);
+    }
+
+    private boolean isFoodC0F() {
+        return this.foodMode.getValue() == 3;
+    }
+
+    @Override
+    public void onDisabled() {
+        if (this.c0fStep != C0FStep.NONE) {
+            this.releaseC0F();
+        }
+        fakeEating = false;
+        this.c0fNoUsingItemTicks = 0;
     }
 
     public boolean isSwordActive() {
@@ -56,14 +90,20 @@ public class NoSlow extends Module {
                 || this.bowMode.getValue() == 2 && ItemUtil.isUsingBow();
     }
 
+    public boolean isC0FActive() {
+        return this.isFoodC0F() && this.c0fStep != C0FStep.NONE;
+    }
+
     public boolean isAnyActive() {
-        return mc.thePlayer.isUsingItem() && (this.isSwordActive() || this.isFoodActive() || this.isBowActive());
+        return this.isC0FActive()
+                || mc.thePlayer.isUsingItem() && (this.isSwordActive() || this.isFoodActive() || this.isBowActive());
     }
 
     public boolean canSprint() {
         return this.isSwordActive() && this.swordSprint.getValue()
                 || this.isFoodActive() && this.foodSprint.getValue()
-                || this.isBowActive() && this.bowSprint.getValue();
+                || this.isBowActive() && this.bowSprint.getValue()
+                || this.isC0FActive() && this.foodSprint.getValue();
     }
 
     public int getMotionMultiplier() {
@@ -79,17 +119,32 @@ public class NoSlow extends Module {
     @EventTarget
     public void onLivingUpdate(LivingUpdateEvent event) {
         if (this.isEnabled() && this.isAnyActive()) {
+            if (this.canSprint() && this.shouldForceSprint()) {
+                mc.thePlayer.setSprinting(true);
+            } else if (!this.canSprint()) {
+                mc.thePlayer.setSprinting(false);
+            }
             float multiplier = (float) this.getMotionMultiplier() / 100.0F;
             mc.thePlayer.movementInput.moveForward *= multiplier;
             mc.thePlayer.movementInput.moveStrafe *= multiplier;
-            if (!this.canSprint()) {
-                mc.thePlayer.setSprinting(false);
-            }
         }
+    }
+
+    private boolean shouldForceSprint() {
+        boolean movingForward = mc.thePlayer.movementInput.moveForward >= 0.8F;
+        boolean hasFood = (float) mc.thePlayer.getFoodStats().getFoodLevel() > 6.0F
+                || mc.thePlayer.capabilities.allowFlying;
+        boolean wantsSprint = mc.gameSettings.keyBindSprint.isKeyDown() || mc.thePlayer.isSprinting();
+        return movingForward
+                && hasFood
+                && wantsSprint
+                && !mc.thePlayer.isSneaking()
+                && !mc.thePlayer.isPotionActive(net.minecraft.potion.Potion.blindness);
     }
 
     @EventTarget(Priority.LOW)
     public void onPlayerUpdate(PlayerUpdateEvent event) {
+        this.handleC0FTick();
         if (this.isEnabled() && this.isFloatMode()) {
             int item = mc.thePlayer.inventory.currentItem;
             if (this.lastSlot != item && PlayerUtil.isUsingItem()) {
@@ -128,6 +183,100 @@ public class NoSlow extends Module {
                 mc.thePlayer.motionY = 0.42F;
             }
         }
+    }
+
+    private void handleC0FTick() {
+        if (!this.isEnabled() || !this.isFoodC0F()) {
+            if (this.c0fStep != C0FStep.NONE) {
+                this.releaseC0F();
+            }
+            this.c0fNoUsingItemTicks = 0;
+            return;
+        }
+
+        boolean usingFood = ItemUtil.isEating() && mc.thePlayer.isUsingItem();
+
+        if (this.c0fStep != C0FStep.EATING) {
+            KeyBindUtil.setKeyBindState(mc.gameSettings.keyBindUseItem.getKeyCode(), false);
+        }
+
+        if (this.c0fStep == C0FStep.NONE && usingFood) {
+            this.c0fStep = C0FStep.CANCEL_C0F;
+            fakeEating = true;
+            this.c0fPackets.clear();
+
+            if (mc.thePlayer.openContainer != mc.thePlayer.inventoryContainer) {
+                PacketUtil.sendPacket(new C0DPacketCloseWindow(mc.thePlayer.openContainer.windowId));
+            }
+        }
+
+        if (this.c0fStep == C0FStep.EATING) {
+            if (mc.thePlayer.isUsingItem()) {
+                this.c0fNoUsingItemTicks = 0;
+            } else {
+                this.c0fNoUsingItemTicks++;
+                if (this.c0fNoUsingItemTicks >= 10) {
+                    this.releaseC0F();
+                }
+            }
+        } else {
+            this.c0fNoUsingItemTicks = 0;
+        }
+    }
+
+    @EventTarget(Priority.LOWEST)
+    public void onPacket(PacketEvent event) {
+        if (!this.isEnabled() || !this.isFoodC0F()) {
+            return;
+        }
+
+        Packet<?> packet = event.getPacket();
+
+        if (event.getType() == EventType.SEND) {
+            if (this.c0fStep != C0FStep.NONE && packet instanceof C0FPacketConfirmTransaction) {
+                event.setCancelled(true);
+                this.c0fPackets.offer(packet);
+
+                if (this.c0fStep == C0FStep.CANCEL_C0F) {
+                    this.c0fStep = C0FStep.SWAP_HANDS;
+                    PacketUtil.sendPacket(new CPacketSwapItemWithOffHand());
+                }
+            }
+
+            if (this.c0fStep == C0FStep.EATING && packet instanceof C07PacketPlayerDigging) {
+                if (((C07PacketPlayerDigging) packet).getStatus() == C07PacketPlayerDigging.Action.RELEASE_USE_ITEM) {
+                    this.releaseC0F();
+                }
+            }
+        }
+
+        if (event.getType() == EventType.RECEIVE) {
+            if (this.c0fStep == C0FStep.SWAP_HANDS && packet instanceof S2FPacketSetSlot) {
+                KeyBindUtil.setKeyBindState(mc.gameSettings.keyBindUseItem.getKeyCode(), true);
+                this.c0fStep = C0FStep.EATING;
+            }
+
+            if (packet instanceof S08PacketPlayerPosLook) {
+                this.releaseC0F();
+            }
+        }
+    }
+
+    private void releaseC0F() {
+        while (!this.c0fPackets.isEmpty()) {
+            Packet<?> packet = this.c0fPackets.poll();
+            if (packet != null && mc.getNetHandler() != null) {
+                PacketUtil.sendPacketNoEvent(packet);
+            }
+        }
+
+        if (mc.getNetHandler() != null) {
+            PacketUtil.sendPacket(new CPacketSwapItemWithOffHand());
+        }
+
+        this.c0fStep = C0FStep.NONE;
+        this.c0fNoUsingItemTicks = 0;
+        fakeEating = false;
     }
 
     @Override
