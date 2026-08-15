95ee7af21643423cca544deabaf9cac707801a11
UnfairGaming <3675967734@qq.com>
2026-08-15 20:14:24 +0800

    FlagDetector更新

--- src/main/java/cn/unfair/module/modules/misc/FlagDetector.java
+++ src/main/java/cn/unfair/module/modules/misc/FlagDetector.java
@@ -1,9 +1,11 @@
 package cn.unfair.module.modules.misc;
 
 import cn.unfair.event.EventTarget;
+import cn.unfair.event.types.EventType;
 import cn.unfair.events.PacketEvent;
 import cn.unfair.module.Module;
 import cn.unfair.util.ChatUtil;
+import net.minecraft.network.play.server.S06PacketUpdateHealth;
 import net.minecraft.network.play.server.S08PacketPlayerPosLook;
 
 public class FlagDetector extends Module {
@@ -13,11 +15,20 @@ public class FlagDetector extends Module {
 
     @EventTarget
     public void onPacket(PacketEvent event) {
-        if (!this.isEnabled())
+        if (!this.isEnabled() || event.getType() != EventType.RECEIVE) {
             return;
+        }
 
         if (event.getPacket() instanceof S08PacketPlayerPosLook) {
             ChatUtil.sendFormatted("&7[&cFlagDetector&7] &fServer flag detected (Lagback)!");
+        } else if (event.getPacket() instanceof S06PacketUpdateHealth packet) {
+            int hunger = packet.getFoodLevel();
+            if (hunger < 0) {
+                ChatUtil.sendFormatted(String.format(
+                        "&7[&cFlagDetector&7] &fServer flag detected (Invalid Hunger: &c%d&f)!",
+                        hunger
+                ));
+            }
         }
     }
 }
