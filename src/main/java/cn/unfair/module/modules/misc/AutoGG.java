045387bb114cd46cbed67108e944a02f9afd9c4e
hhhhh <jincheng20032022@163.com>
2026-08-17 16:36:09 +0800

    AutoGG

---
+++ src/main/java/cn/unfair/module/modules/misc/AutoGG.java
@@ -0,0 +1,60 @@
+package cn.unfair.module.modules.misc;
+
+import cn.unfair.event.EventTarget;
+import cn.unfair.event.types.EventType;
+import cn.unfair.events.PacketEvent;
+import cn.unfair.module.Module;
+import cn.unfair.property.properties.ModeProperty;
+import cn.unfair.util.ChatUtil;
+import cn.unfair.util.TimerUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.play.server.S45PacketTitle;
+
+public class AutoGG extends Module {
+    private static final Minecraft mc = Minecraft.getMinecraft();
+
+    public final ModeProperty mode = new ModeProperty("Mode",0,new String[]{"Hypixel"});
+
+    private final TimerUtil timer = new TimerUtil();
+
+    public AutoGG() {
+        super("AutoGG", false);
+    }
+
+    @EventTarget
+    public void onPacket(PacketEvent event) {
+        if (!this.isEnabled() || event.getType() != EventType.RECEIVE) {
+            return;
+        }
+
+        if (!(event.getPacket() instanceof S45PacketTitle)) {
+            return;
+        }
+
+        S45PacketTitle packet = (S45PacketTitle) event.getPacket();
+        if (packet.getType() != S45PacketTitle.Type.TITLE || packet.getMessage() == null) {
+            return;
+        }
+
+        String title = packet.getMessage().getUnformattedText();
+        if(!title.toUpperCase().contains("VICTORY") && !title.contains("胜利")) {
+            return;
+        }
+
+        if (!this.timer.hasTimeElapsed(15000L)) {
+            return;
+        }
+        this.timer.reset();
+
+        mc.addScheduledTask(() -> {
+            if (this.isEnabled()) {
+                ChatUtil.sendMessage("GG");
+            }
+        });
+    }
+
+    @Override
+    public String[] getSuffix() {
+        return new String[]{this.mode.getModeString()};
+    }
+}
