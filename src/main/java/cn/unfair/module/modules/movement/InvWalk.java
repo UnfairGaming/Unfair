616dff03e471dbb8eb2ea47a44a7072e73d36941
UnfairGaming <3675967734@qq.com>
2026-08-23 17:44:51 +0800

    删除傻逼乌鸦cgui

--- src/main/java/cn/unfair/module/modules/movement/InvWalk.java
+++ src/main/java/cn/unfair/module/modules/movement/InvWalk.java
@@ -11,7 +11,6 @@ import cn.unfair.module.Module;
 import cn.unfair.property.properties.BooleanProperty;
 import cn.unfair.property.properties.ModeProperty;
 import cn.unfair.ui.clickgui.augustus.AugustusClickGui;
-import cn.unfair.ui.clickgui.raven.RavenClickGui;
 import cn.unfair.util.KeyBindUtil;
 import cn.unfair.util.PacketUtil;
 import net.minecraft.client.Minecraft;
@@ -93,7 +92,7 @@ public class InvWalk extends Module {
     public void onUpdate(UpdateEvent event) {
         if (!this.isEnabled() || event.getType() != EventType.PRE) return;
 
-        if ((mc.currentScreen instanceof RavenClickGui || mc.currentScreen instanceof AugustusClickGui) && this.guiEnabled.getValue()) {
+        if ((mc.currentScreen instanceof AugustusClickGui) && this.guiEnabled.getValue()) {
             pressMovementKeys();
             return;
         }
--- src/main/java/cn/unfair/module/modules/render/ClickGui.java
+++ src/main/java/cn/unfair/module/modules/render/ClickGui.java
@@ -4,14 +4,12 @@ import cn.unfair.event.EventManager;
 import cn.unfair.module.Module;
 import cn.unfair.property.properties.ModeProperty;
 import cn.unfair.ui.clickgui.augustus.AugustusClickGui;
-import cn.unfair.ui.clickgui.raven.RavenClickGui;
 import net.minecraft.client.Minecraft;
 import org.lwjgl.input.Keyboard;
 
 public class ClickGui extends Module {
     private static final Minecraft mc = Minecraft.getMinecraft();
-    public final ModeProperty mode = new ModeProperty("Mode", 1, new String[]{"Raven", "Augustus"});
-    private RavenClickGui ravenClickGui;
+    public final ModeProperty mode = new ModeProperty("Mode", 1, new String[]{"Augustus"});
     private AugustusClickGui augustusClickGui;
 
     public ClickGui() {
@@ -23,13 +21,6 @@ public class ClickGui extends Module {
     public void onEnabled() {
         setEnabled(false);
         if (mode.getValue() == 0) {
-            if (ravenClickGui == null) {
-                ravenClickGui = new RavenClickGui();
-                EventManager.register(ravenClickGui);
-                ravenClickGui.initMain();
-            }
-            mc.displayGuiScreen(ravenClickGui);
-        } else if (mode.getValue() == 1) {
             if (augustusClickGui == null) {
                 augustusClickGui = new AugustusClickGui();
                 EventManager.register(augustusClickGui);
